e0aaf6f145c6cd13dd34e1aaa6a613cff7d7e0b3
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-11-14 00:39:39 +0300

    Completed assigment

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -8,6 +8,14 @@ function ExampleComponent() {
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
+       <img
+        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        alt="a deeply impressed man, perhaps thinking 'whoa'"
+      />
+       <img
+        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        alt="a deeply impressed man, perhaps thinking 'whoa'"
+      />
       <p>Whoa!</p>
     </div>
   );
